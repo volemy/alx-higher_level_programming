f927ca9f129152b674309683ffec9b391ddf0416
volemy <minayobenevolence0@gmail.com>
2023-10-05 21:28:23 +0000

    program that imports functions from the file calculator_1.py, does some Maths, and prints the result

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
 a = 10
 b = 5
 
-print(a, "+", b, "=", add(a, b))
-print(a, "-", b, "=", sub(a, b))
-print(a, "*", b, "=", mul(a, b))
-print(a, "/", b, "=", div(a, b))
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
