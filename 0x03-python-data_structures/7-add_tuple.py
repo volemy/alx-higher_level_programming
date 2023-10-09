1c99a9701c1e22e6f43185f6a37ae1c32e913b91
volemy <minayobenevolence0@gmail.com>
2023-10-09 19:47:31 +0000

    function that adds 2 tuples

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    if not tuple_a and not tuple_b:
+        return (0, 0)
+
+    if len(tuple_a) < 2:
+        tuple_a = tuple_a + (0,) * (2 -len(tuple_a))
+    if len(tuple_b) < 2:
+        tuple_b = tuple_b + (0,) * (2 - len(tuple_b))
+
+    if len(tuple_a) > 2:
+        tuple_a = tuple_a[:2]
+    if len(tuple_b) > 2:
+        tuple_b = tuple_b[:2]
+
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
