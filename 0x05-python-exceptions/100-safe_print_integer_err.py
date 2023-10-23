3c0e949b641757c22f9284dee50d79a063a78b11
volemy <minayobenevolence0@gmail.com>
2023-10-23 19:09:10 +0000

    safe integer print task 7

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from sys import stderr
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except Exception as exc:
+        print("Exception: {}".format(exc), file=stderr)
+        return False
