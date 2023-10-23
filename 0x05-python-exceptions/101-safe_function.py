c88db169907325a9973224f3ef2307765cdb052a
volemy <minayobenevolence0@gmail.com>
2023-10-23 20:14:47 +0000

    safe function

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from sys import stderr
+
+
+def safe_function(fct, *args):
+    try:
+        output = fct(*args)
+        return output
+    except Exception as e:
+        print("Exception: {}".format(e), file=stderr)
+        return None
