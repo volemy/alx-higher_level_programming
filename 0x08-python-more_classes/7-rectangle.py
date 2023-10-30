9bd59dc2abc174ff3bfe77b65c90960e82b5d0d7
volemy <minayobenevolence0@gmail.com>
2023-10-30 21:45:24 +0000

    task 7

---
+++ 0x08-python-more_classes/7-rectangle.py
@@ -0,0 +1,148 @@
+#!/usr/bin/python3
+"""
+Rectangle class
+this module has a class that defines a rectangle
+"""
+
+
+class Rectangle:
+    """
+    Rectangle Class.
+    This class defines a rectangle.
+
+    Attributes:
+        width (int): Width of the rectangle.
+        height (int): Height of the rectangle.
+        number_of_instances (int): number of instances created and not deleted
+
+    Methods:
+        __init__(self, width=0, height=0)
+        width(self)
+        width(self, value)
+        height(self)
+        height(self, value)
+        area(self)
+        perimeter(self)
+        __str__self
+        __repr__self
+        __del__self
+    """
+
+    number_of_instances = 0
+    print_symbol = "#"
+
+    def __init__(self, width=0, height=0):
+        """
+        Initializes a Rectangle object with width and height.
+
+        Args:
+            width (int, optional): The width of the rectangle. Defaults to 0.
+            height (int, optional): The height of the rectangle. Defaults to 0.
+        """
+        self.width = width
+        self.height = height
+        type(self).number_of_instances += 1
+
+    @property
+    def width(self):
+        """
+        Getter method for the width attribute.
+
+        Returns:
+            int: The width of the rectangle.
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        Setter method for the width attribute.
+
+        Args:
+            value (int): The width to set.
+
+        Raises:
+            TypeError: If the width is not an integer.
+            ValueError: If the width is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """
+        Getter method for the height attribute.
+
+        Returns:
+            int: The height of the rectangle.
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        Setter method for the height attribute.
+
+        Args:
+            value (int): The height to set.
+
+        Raises:
+            TypeError: If the height is not an integer.
+            ValueError: If the height is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+
+        self.__height = value
+
+    def area(self):
+        """
+        Calculates and returns the area of the rectangle.
+
+        Returns:
+            int: The area of the rectangle.
+        """
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """
+        Calculates and returns the perimeter of the rectangle.
+
+        Returns:
+            int: The perimeter of the rectangle.
+        """
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (2 * self.__width) + (2 * self.__height)
+
+    def __str__(self):
+        """
+        Returns string of rectangle instance using the # character
+        """
+        if self.__height == 0 or self.width == 0:
+            return ""
+        rectangle_image = ""
+        for i in range(self.__height):
+            for j in range(self.__width):
+                rectangle_image += str(self.print_symbol)
+            rectangle_image += "\n"
+        return rectangle_image[:-1]
+
+    def __repr__(self):
+        """
+        Returns string of rectangle instance to recreate a new
+        instance using eval()
+        """
+        return "Rectangle({}, {})".format(self.__width, self.__height)
+
+    def __del__(self):
+        """
+        This prints a specific message for del
+        """
+        print("Bye rectangle...")
+        type(self).number_of_instances -= 1
