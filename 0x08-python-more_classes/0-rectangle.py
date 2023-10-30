83f39b3d87c229b0349311551b6bc7976da7b1e9
volemy <minayobenevolence0@gmail.com>
2023-10-30 13:20:34 +0000

    simple rectangle

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
+"""
+Rectangle class.
+this module contains an empty class that defines rectangle
+"""
+
+
 class Rectangle:
+    """
+    This class defines an empty rectangle class
+    """
+
     pass
