8eacb7d783a6350164fb00ae27c6f1199350f8cc
volemy <minayobenevolence0@gmail.com>
2023-11-07 17:53:52 +0000

    task 13

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
 """
-This inserts line of text to a file
+This inserts line of text to a file.
 """
 
 
 def append_after(filename="", search_string="", new_string=""):
     """
-    This appends the new string after search_string in filename
+    This appends the new string after search_string in filename.
     """
 
-    with open(filename, mode="r") as file:
+    with open(filename, mode="r") as inpu_file:
         text = file.readlines()
 
-    with open(filename, mode="w") as file_to_write:
+    with open(filename, mode="w") as output_file:
         s =""
         for line in text:
             s += line
             if search_string in line:
                 s += new_string
-        file_to_write.write(s)
+        output_file.write(s)
