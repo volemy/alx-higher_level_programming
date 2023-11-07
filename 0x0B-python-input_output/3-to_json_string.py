5702cce3b10cad19cdb3111e07ee1d9b2faa5bd5
volemy <minayobenevolence0@gmail.com>
2023-11-07 13:42:29 +0000

    task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This returns the JSON representation of an object
+"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """
+    Returns the JSON representation of an object(string)
+    """
+    return json.dumps(my_obj)
