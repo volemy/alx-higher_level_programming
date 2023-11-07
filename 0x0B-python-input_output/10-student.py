f1a9d228d0d31a90e9a60f2cdbd3367eedc66727
volemy <minayobenevolence0@gmail.com>
2023-11-07 20:30:30 +0000

    task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+This module creates a student class
+"""
+
+
+class Student:
+    """
+    This class defines a student
+    public attributes:
+        - first_name
+        _ last_name
+        _ age
+
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        instantiation
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        This returns a dictionary representation of an instance
+        """
+
+        my_dict = dict()
+        if type(attrs) is list and all(type(attr) is str for attr in attrs):
+            for attr in attrs:
+                if attr in self.__dict__:
+                    my_dict.update({attr: self.__dict__[attr]})
+            return my_dict
+        return self.__dict__.copy()
+
