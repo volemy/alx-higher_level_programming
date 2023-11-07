26c4b1e22f62f43bc2dacccfa7d7078d703bf72f
volemy <minayobenevolence0@gmail.com>
2023-11-07 21:05:46 +0000

    task 14

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,41 +1,52 @@
 #!/usr/bin/python3
 """
-This writes a script that reads line by line computer metrics
+Reads from standard input and computes metrics.
 """
 
 
-import sys
-
-total_file_size = 0
-status_codes = {}
-valid_codes = {'200', '301', '400', '401', '403', '404', '405', '500'}
-lines = []
-
-
-def print_stats():
+def print_metrics(total_size, code_counts):
     """
-    This prints accumulated metrics
+    Print accumulated metrics.
     """
-    print("File size: {}".format(total_file_size))
-    for code in sorted(valid_codes):
-        count = status_codes.get(code, 0)
-        if count > 0:
-            print("{}: {}".format(code, count))
-
-try:
-    for line in sys.stdin:
-        lines.append(line.strip())
-        parts = line.split()
-        if len(parts) >= 7:
-            total_file_size += int(parts[-1])
-            status_code = parts[-2]
-            if status_code in valid_codes:
-                status_codes[status_code] = status_codes.get(status_code, 0) + 1
+    print("Total file size: {}".format(total_size))
+    for key in sorted(code_counts):
+        print("{}: {}".format(key, code_counts[key]))
 
 
-        if len(lines) == 10:
-            print_stats()
-            lines = []
+if __name__ == "__main__":
+    import sys
 
-except KeyboardInterrupt:
-    print_stats()
+    total_size = 0
+    status_codes = {}
+    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+    line_count = 0
+
+    try:
+        for line in sys.stdin:
+            if line_count == 10:
+                print_metrics(total_size, status_codes)
+                line_count = 1
+            else:
+                line_count += 1
+
+            parts = line.split()
+
+            try:
+                total_size += int(parts[-1])
+            except (IndexError, ValueError):
+                pass
+
+            try:
+                if parts[-2] in valid_codes:
+                    if status_codes.get(parts[-2], -1) == -1:
+                        status_codes[parts[-2]] = 1
+                    else:
+                        status_codes[parts[-2]] += 1
+            except IndexError:
+                pass
+
+        print_metrics(total_size, status_codes)
+
+    except KeyboardInterrupt:
+        print_metrics(total_size, status_codes)
+        raise
