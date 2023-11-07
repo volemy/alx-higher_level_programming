2a51ac275c5ae0508541d16ca014fc1a6f98032e
volemy <minayobenevolence0@gmail.com>
2023-11-07 13:00:32 +0000

    task 2

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This appends a string at the end of a text file
+"""
+
+def append_write(filename="", text=""):
+    """
+    This appends a string to the end of a text file in UTF-8 encoding
+    and retruns the number of characters addded.
+    """
+
+    with open(filename, mode='a', encoding='utf-8') as file:
+        num_characters_added = file.write(text)
+
+    return num_characters_added
