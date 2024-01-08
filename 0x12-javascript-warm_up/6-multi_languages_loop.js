c655d200ee8db7fae2324b8c0049b58b3a5e66a7
volemy <minayobenevolence0@gmail.com>
2024-01-08 20:19:48 +0000

    task 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const languages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+let index = 0;
+
+while (index < languages.length) {
+ console.log(languages[index]);
+ index++;
+}
