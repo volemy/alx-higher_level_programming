b76a270e061917c324ac2903b4642384a624fb2e
volemy <minayobenevolence0@gmail.com>
2024-01-14 12:55:16 +0000

    task 4

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+A script that lists all cities sorted by city ID
+"""
+
+import MySQLdb
+from sys import argv
+
+
+def list_cities():
+    """
+    This lists ciities from mentioned database sorted by city ID
+    """
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+
+    cursor = db.cursor()
+
+    query = """
+    SELECT cities.id, cities.name, states.name
+    FROM cities
+    JOIN states ON cities.state_id = states.id
+    ORDER BY cities.id ASC
+    """
+
+    cursor.execute(query)
+
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    list_cities()
