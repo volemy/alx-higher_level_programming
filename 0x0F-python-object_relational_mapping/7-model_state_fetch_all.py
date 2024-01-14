830f146865a380afbd5d3786c00a9b5251b04751
volemy <minayobenevolence0@gmail.com>
2024-01-14 20:41:04 +0000

    task 7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -2,6 +2,8 @@
 """
 This Lists all states from the database using SQLAlchemy
 """
+
+
 from sys import argv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
@@ -10,10 +12,13 @@ from model_state import Base, State
 
 def list_all_states():
     """
-    This Connect to the database and list all states
+    This connects to the database and list all states
+    Returns: None
     """
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(argv[1],
                                                                        argv[2], argv[3]), pool_pre_ping=True)
+
+
 
     Base.metadata.create_all(engine)
 
