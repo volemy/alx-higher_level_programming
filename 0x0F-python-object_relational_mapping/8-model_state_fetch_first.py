cf6dade58e52aac6f997ebd0ed50efe01f7843cb
volemy <minayobenevolence0@gmail.com>
2024-01-14 20:54:39 +0000

    task 8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,23 +1,32 @@
 #!/usr/bin/python3
 """
-Script that prints the first state from the database using SQLAlchemy
+This script prints the first State object from the database using SQLAlchemy.
 """
 
 from sys import argv
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Sessionmaker
+from sqlalchemy.orm import Session
 from model_state import Base, State
 
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(argv[1], argv[2], argv[3]),
-                           pool_pre_ping=True)
+def list_first_state():
+    """
+    Connects to the database and prints the first State object.
+    """
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(argv[1],
+                                                                       argv[2], argv[3]), pool_pre_ping=True)
+
     Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    state = session.query(State).order_by(State.id).first()
-    if state:
-        print("{}: {}".format(state.id, state.name))
+
+    session = Session(engine)
+
+    first_state = session.query(State).order_by(State.id).first()
+
+    if first_state:
+        print(f"{first_state.id}: {first_state.name}")
     else:
         print("Nothing")
+
     session.close()
+
+if __name__ == "__main__":
+    list_first_state()
