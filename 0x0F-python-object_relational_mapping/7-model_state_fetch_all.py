cbbf118cea95ad7acc9b6027c9019854ca6b53d2
volemy <minayobenevolence0@gmail.com>
2024-01-14 20:31:49 +0000

    task 7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -15,6 +15,7 @@ def list_all_states():
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(argv[1],
                                                                        argv[2], argv[3]), pool_pre_ping=True)
 
+    Base.metadata.create_all(engine)
 
     session = Session(engine)
 
