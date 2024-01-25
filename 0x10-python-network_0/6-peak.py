b9d450bb48cdc9d45af37b4dc3007e6a48e0da7d
volemy <minayobenevolence0@gmail.com>
2024-01-25 19:18:33 +0000

    task6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def find_peak(list_of_integers):
+    """
+    Find a peak in list of unsorted integers
+    """
+    if not list_of_integers:
+        return None
+
+    left, right = 0, len(list_of_integers) - 1
+
+    while left < right:
+        mid = (left + right) // 2
+
+        if list_of_integers[mid] < list_of_integers[mid + 1]:
+            left = mid + 1
+        else:
+            right = mid
+    return list_of_integers[left]
