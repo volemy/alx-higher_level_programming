dc38f015633243a675ba5a7741533d0f5045d3e7
volemy <minayobenevolence0@gmail.com>
2024-01-26 23:29:50 +0000

    task 6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Script that takes URL and email address sends POST request to the passed URL
+"""
+import requests
+import sys
+
+
+def send_post_request(url, email):
+    data = {'email': email}
+    response = requests.post(url, data=data)
+    body = response.content
+    print("Body response:")
+    print("     -type: <class 'bytes'>")
+    print("     -content: {}".format(body))
+    print("     -utf8 content: {}".format(body.decode('utf-8')))
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python3 send_post_request.py <URL> <EMAIL>")
+        sys.exit(1)
+    url = sys.argv[1]
+    email = sys.argv[2]
+    send_post_request(url, email)
