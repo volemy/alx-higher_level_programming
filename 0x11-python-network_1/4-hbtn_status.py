63c7f4cfc28257681d491e481807a188d5219e62
volemy <minayobenevolence0@gmail.com>
2024-01-26 21:57:01 +0000

    task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -9,12 +9,12 @@ def get_status():
     """
     script that fetches https://alx-intranet.hbtn.io/status
     """
-    response = requests.get('https://alx-intranet.hbtn.io/status')
-    body = response.content
+    url = 'https://alx-intranet.hbtn.io/status'
+    response = requests.get(url)
+    body = response.text
     print("Body response:")
-        print("     -type: <class 'bytes>'"
-        print("     -content: {}".format(body))
-        print("     -utf8 content: {}".format(body.decode('utf8_content'))
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
 
 if __name__ == "__main__":
     get_status()
