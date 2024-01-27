48c2444c344e3167fb01b9ecace13a3135c553b2
volemy <minayobenevolence0@gmail.com>
2024-01-27 00:00:34 +0000

    task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -6,33 +6,27 @@ letter as a parameter.
 """
 import requests
 import sys
-import json
 
-def search_user(x):
+def search_user(letter):
     """
-    Python script that takes in a letter and sends a
-    POST request to http://0.0.0.0:5000/search_user with the
-    letter as a parameter.
+    script that takes in a letter and sends a POST
+    request to http://0.0.0.0:5000/search_user
     """
-    if x is None:
-        x = ""
-    data = {'x': x}
+    if not letter:
+        letter = ""
+
+    data = {'q': letter}
     response = requests.post('http://0.0.0.0:5000/search_user', data=data)
-    response_body = response.content.decode('utf-8')
+
     try:
-        response_json = json.loads(response_body)
+        response_json = response.json()
         if response_json:
             print("[{}] {}".format(response_json['id'], response_json['name']))
         else:
             print("No result")
-    except json.JSONDecodeError:
+    except ValueError:
         print("Not a valid JSON")
 
 if __name__ == "__main__":
-    if len(sys.argv) > 2:
-        print("Usage: python3 8-json_api.py [letter]")
-        sys.exit(1)
-    elif len(sys.argv) == 2:
-        search_user(sys.argv[1])
-    else:
-        search_user(None)
+    letter = sys.argv[1] if len(sys.argv) == 2 else ""
+    search_user(letter)
