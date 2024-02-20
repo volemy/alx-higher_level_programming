e4652a88b913aa37a6d4040d698b78da1e43616a
volemy <minayobenevolence0@gmail.com>
2024-02-20 15:36:28 +0000

    task 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,16 +1,13 @@
 #!/usr/bin/node
 
 const request = require('request');
-const episodeNum = process.argv[2];
-const API_URL = 'https://swapi-api.hbtn.io/api/films/';
+const id = process.argv[2];
 
-request(API_URL + episodeNum, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else if (response.statusCode === 200) {
-    const responseJSON = JSON.parse(body);
-    console.log(responseJSON.title);
+request.get(`https://swapi-api.alx-tools.com/api/films/${id}`, (error, response, body) => {
+  if (error) {
+    console.error(error);
   } else {
-    console.log('Error code: ' + response.statusCode);
+    const movie = JSON.parse(body);
+    console.log(`Title: ${movie.title}`);
   }
 });
